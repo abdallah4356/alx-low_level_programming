0911e02c47802cdeebb70f991637e51bed7318ad
abdallah <abdallahelmohammady@gmail.com>
2023-06-22 18:13:05 -0700

    shf

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,7 +11,7 @@
  */
 
 int largest_number(int a, int b, int c)
-	{
+{
 	int largest;
 	if (a >= b && a >= c)
 	{
@@ -27,4 +27,4 @@ int largest_number(int a, int b, int c)
 	}
 
 	return (largest);
-	}
+}
