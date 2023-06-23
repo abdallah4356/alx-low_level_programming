a11c9ec29c42d015c9ac2945f964eb88ce2cde3d
abdallah <abdallahelmohammady@gmail.com>
2023-06-23 07:23:01 -0700

    hfs

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Description: checking if c is uppercase
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c);
+{
+if (c >= 65 && c <= 90)
+{
+	c = 1;
+}
+	else
+	{
+		c = 0;
+	}
+}
