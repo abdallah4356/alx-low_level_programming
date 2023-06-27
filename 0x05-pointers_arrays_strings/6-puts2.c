bd3023302e629195439cac8a9be7e9bfe0963974
abdallah <abdallahelmohammady@gmail.com>
2023-06-26 19:31:52 -0700

    hf

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ *
+ * @str: a string
+ *
+ * Return: string of characters
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
