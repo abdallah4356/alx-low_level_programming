de8a58a75a5ada336049d5bdad551372a4a554f6
abdallah <abdallahelmohammady@gmail.com>
2023-06-27 19:49:49 -0700

    SHf

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates a random password
+ *
+ * @:
+ *
+ *
+*/
+
+int main(void)
+{
+	int len = 8;
+
+	char password[len + 1];
+	srand(time(NULL));
+
+	while()
+}
