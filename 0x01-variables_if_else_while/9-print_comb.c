b98492b6d067079314867191f66ebc4450436e83
abdallah <abdallahelmohammady@gmail.com>
2023-07-02 15:29:43 -0700

    SHD

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,11 +12,16 @@ int main(void)
 {
 	int n = 48;
 
-	while (n <= 56)
+	while (n <= 57)
 	{
 		putchar(n);
-		putchar(44);
-		putchar(32);
+
+		if (n != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+
 		n++;
 	}
 	putchar('\n');
