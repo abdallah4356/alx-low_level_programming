9d164cae1c8517e2817a37b82a2ad0088ceb7cbd
abdallah <abdallahelmohammady@gmail.com>
2023-07-02 02:10:11 -0700

    shdf

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alpha in reverse
+ *
+ * Description: a C function that prints the alphabet in reverse
+ *
+ * Return: 0 Always success
+*/
+
+int main(void)
+{
+	int num = 'z';
+
+	while (num >= 'a')
+	{
+		putchar(num);
+		num++;
+	}
+		putchar('\n');
+	return (0);
+}
