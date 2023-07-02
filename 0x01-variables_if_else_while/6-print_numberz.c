3b943224b8f9be038f5ebcb607de3908434a777b
abdallah <abdallahelmohammady@gmail.com>
2023-07-02 02:03:19 -0700

    fh

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits
+ *
+ * Description: prints all digits from 0 to 9
+ *
+ * Return: 0 always success
+*/
+
+int main(void)
+{
+	int u = '0';
+
+	while (u <= '0')
+	{
+		putchar(u);
+		u++;
+	}
+	putchar('\n');
+	return (0);
+}
+
