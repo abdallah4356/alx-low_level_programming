3d5ef5f385d7f24d328e6756cd1a9d65cfccb1f7
abdallah <abdallahelmohammady@gmail.com>
2023-07-04 00:13:20 -0700

    Shfd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - adding two integers
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: sum of the two integers
+*/
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = (a + b);
+
+	_putchar(sum + 48);
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#inlcude "main.h"
+
+/**
+ * jack_bauer - prints every minute
+ *
+ * Description: 
+ *
+ * Return:
+*/
+
+void jack_bauer(void)
+{
+	int q;
+	int w;
+	int e;
+	int r;
+
+	for (q = 0; q <= 9; q++)
+	{
+		_putchar();
+	}
+}
