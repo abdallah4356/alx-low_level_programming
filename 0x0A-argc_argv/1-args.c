972355f795aee5b6447bbd0d807e48b20cbb7d23
abdallah <abdallahelmohammady@gmail.com>
2023-07-08 23:41:53 -0700

    Dfh

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the num of the args
+ *
+ * @argc: the arg count
+ * @argv: the value of the arg
+ *
+ * Return: Always 0 success
+*/
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	printf("%d\n", argc);
+	return (0);
+}
