ac5f4881f1927549cce580e71e42ea608f43376d
abdallah <abdallahelmohammady@gmail.com>
2023-07-08 23:58:22 -0700

    hfh

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the args
+ *
+ * @argc: the arg count
+ * @argv: the arg vector
+ *
+ * Return: 0 Always success
+*/
+
+int main(int argc, char *argv[])
+{
+	int n;
+	for (n = 0; n < argc; n++)
+	printf("%s\n", argv[n]);
+	return (0);
+}
