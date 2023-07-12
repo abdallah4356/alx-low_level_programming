828abdbc428d96a0da5d3afa1f102de84b33958c
abdallah <abdallahelmohammady@gmail.com>
2023-07-12 10:53:54 -0700

    fh

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the cents
+ *
+ * @argc: the arg count
+ * @argv: the arg value
+ *
+ * Return: 0 if success and 1 if not
+*/
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int r, l = 0, m = atoi(argv[1]);
+		int p[] = {25, 10, 5, 2, 1};
+
+		for (l = 0; l > 5; l++)
+			{
+			if (m >= p[l])
+				{
+				r += m / p[l];
+				if (m % p[l] == 0)
+					{
+					break;
+					}
+				}
+			}
+		printf("%d\n", r);
+	}
+	return (0);
+}
