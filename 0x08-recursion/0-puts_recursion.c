e98d0e38f7779f0dcbeeb106f4bc8d6f06f64b29
abdallah4356 <abdallahelmohammady@gmail.com>
2023-08-02 17:43:34 -0700

    f

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function
+ *
+ * @s: the pointer to the character
+ *
+ * Return: the whole string
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
