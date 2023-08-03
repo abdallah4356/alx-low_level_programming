0ee01a8fd8ea168d8622661c31ecba3ddfc4b1c4
abdallah4356 <abdallahelmohammady@gmail.com>
2023-08-02 17:55:01 -0700

    Fh

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,12 +12,11 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return;
+		_putchar('\n');
 	}
 	else
 	{
-		_puts_recursion(s + 1);
 		_putchar(*s);
-		_putchar('\n');
+		_puts_recursion(s + 1);
 	}
 }
