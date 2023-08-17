95754b67a3eb0c3e49f7f0e8dcad5d50e8939731
abdallah4356 <abdallahelmohammady@gmail.com>
2023-08-16 17:34:52 -0700

    FH

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,13 +4,14 @@
 /**
  * print_name - prints a name
  *
- * @name: the name pointer
- * @f: the pointer to the function
+ * @name: the name
+ * @f: the pointer to the function that is printing a name
  *
  * Return: a pointer to the function
 */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name && f)
 	f(name);
 }
