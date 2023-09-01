5a4ab4da368b6e8ff776356e44a4f4f133068039
abdallah4356 <abdallahelmohammady@gmail.com>
2023-09-01 14:17:09 -0700

    GH

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds all given +numbers
+ *
+ * @argc: the argument counter
+ * @argv: the argument vector
+ *
+ * Return: 0 Always success
+*/
+
+int main(int argc, char *argv[])
+{
+	while (--argc)
+	{
+		int m = 0;
+		char *u;
+
+		for (u = argv[argc]; *u; u++)
+			if (*u < 48 || *u > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				m += atoi(argv[argc]);
+			}
+	}
+	printf("%d\n", m);
+	return (0);
+}
