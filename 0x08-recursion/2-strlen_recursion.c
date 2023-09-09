535e895e6ed360124882f268e01daf3ac9438c3f
abdallah4356 <abdallahelmohammady@gmail.com>
2023-09-09 15:32:23 -0700

    GH

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts the length of the string
+ *
+ * @s: the ptr of the first char
+ *
+ * Return: the len of the stir
+*/
+
+int _strlen_recursion(char *s)
+{
+	int x = 0;
+
+	if (*s != '\0')
+	{
+		_strlen_recursion(s + 1);
+		x++;
+	}
+	return (x);
+}
