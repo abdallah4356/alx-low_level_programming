7c8f1626aec0126f7ed1e044531d27e7b7e9b210
abdallah4356 <abdallahelmohammady@gmail.com>
2023-09-11 15:23:51 -0700

    GHH

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
-#define (struct dog{
-	*char name;
+/**
+ * struct dog - dog's data structure
+ *
+ * @name: the name of the dog
+ * @age: dog's age
+ * @owner: the owner's name
+ *
+ * Return: a template
+*/
+
+struct dog{
+	char *name;
 	float age;
-	*char owner;
-	};)
+	char *owner;
+	};
+
 #endif
