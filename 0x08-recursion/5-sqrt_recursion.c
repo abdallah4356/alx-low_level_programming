7e02af659a1a392af048aa6261efa8fcc28bc5b8
abdallah4356 <abdallahelmohammady@gmail.com>
2023-09-12 06:16:50 -0700

    GH

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,10 +10,25 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0 || n % 2 != 0)
-		return (-1);
+	return (gt(n, 1));
+}
+
+/**
+ * gt - gets the square root
+ *
+ * @n: the number to get sqrt of
+ * @p: the base number
+ *
+ * Return: the square root of the number
+*/
+
+int gt(int n, int p)
+{
+	if (p * p == n)
+		return (p);
+	else if (p * p < n)
+		return (gt(n, p + 1));
+
 	else
-	{
-		
-	}
+	return (-1);
 }
